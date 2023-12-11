05ccaeb7171ff7de70880fdb7e6c12eaa68922cb
dcorto <daviscorto@gmail.com>
2023-12-11 22:08:01 +0100

    f***ck day 3 xd

--- code/Day3/SolutionA.php
+++ code/Day3/SolutionA.php
@@ -21,13 +21,19 @@ class SolutionA extends Common {
                 $current = $matrix[$i][$j];        
                 if(in_array($current, self::SYMBOLS)){
                     //Look up
-                    echo "Found symbol: $current at position $i $j" . PHP_EOL;
+                    //echo "Found symbol: $current at position $i $j" . PHP_EOL;
                     //find same line:
-                    $solution += $this->findSameLine($matrix, $i, $j);
+                    //$solution += $this->findSameLine($matrix, $i, $j);
+                    
                     //find upper line:
-                    $solution += $this->findUpperLine($matrix, $i, $j);
+                    //$pepe = $this->findUpperLine($matrix, $i, $j);
+                    //echo "pepe $pepe" . PHP_EOL;
+                    $solution += $pepe;
+                    
                     //find lower line:
-                    $solution += $this->findLowerLine($matrix, $i, $j);
+                    $pepe = $this->findLowerLine($matrix, $i, $j);
+                    echo "pepe $pepe" . PHP_EOL;
+                    $solution += $pepe;
                 }
             }
         }
@@ -65,8 +71,8 @@ class SolutionA extends Common {
             break;
         }
 
-        $digitsL = "";
         //to left
+        $digitsL = "";
         for($i=$col;$i>=0;$i--){
             $current = $matrix[$line][$i];
             if(in_array($current, self::SYMBOLS)) {
@@ -75,7 +81,7 @@ class SolutionA extends Common {
             }
             if($current != self::EMPTY) {
                 $digitsL = $current . $digitsL;
-                echo "digitsL $digitsL" . PHP_EOL;
+                //echo "digitsL $digitsL" . PHP_EOL;
                 continue;
             }
             break;
@@ -86,12 +92,108 @@ class SolutionA extends Common {
 
     private function findUpperLine(array $matrix, int $line, int $col): int
     {
-        return 0;
+        
+        if($line <= 0) {
+            return 0;
+        }
+
+        $currentLine = ($line-1);
+        $currentCol = $col;
+        $positions = [];
+        //to right
+        $digitsR = "";
+        for($i=$col;$i<count($matrix[$currentLine]);$i++){
+            $current = $matrix[$currentLine][$i];
+            if(in_array($current, self::SYMBOLS)) {                
+                continue;
+            }
+            if($current != self::EMPTY && !in_array($i, $positions)) {
+                $digitsR .= $current;
+                $positions[] = $i;
+                echo "digitsR $digitsR" . PHP_EOL;
+                continue;
+            }
+            if($i === $col) {
+                continue;
+            }
+            break;
+        }
+
+        //to left
+        $digitsL = "";
+        for($i=$col;$i>=0;$i--){
+            $current = $matrix[$currentLine][$i];
+            if(in_array($current, self::SYMBOLS)) {                
+                continue;
+            }
+            if($current != self::EMPTY && !in_array($i, $positions)) {
+                $digitsL = $current . $digitsL;
+                $positions[] = $i;
+                echo "digitsL $digitsL" . PHP_EOL;
+                continue;
+            }
+            if($i === $col) {
+                continue;
+            }
+
+            break;
+        }
+
+        return intval($digitsR) + intval($digitsL);
     }
 
     private function findLowerLine(array $matrix, int $line, int $col): int
     {
-        return 0;
+        if($line >= count($matrix)) {
+            return 0;
+        }
+
+        $currentLine = ($line+1);
+        $currentCol = $col;
+        $positions = [];
+
+        //to right
+        $digitsR = "";
+        for($i=$col;$i<count($matrix[$currentLine]);$i++){
+            $current = $matrix[$currentLine][$i];
+            if(in_array($current, self::SYMBOLS)) {                
+                continue;
+            }
+            if($current != self::EMPTY && !in_array($i, $positions)) {
+                $digitsR .= $current; 
+                $positions[] = $i;
+                echo "digitsR $digitsR" . PHP_EOL;               
+                continue;
+            }
+            if($i === $col) {
+                continue;
+            }
+
+            break;
+        }
+
+        //to left
+        $digitsL = "";
+        for($i=$col;$i>=0;$i--){
+            $current = $matrix[$currentLine][$i];
+            if(in_array($current, self::SYMBOLS)) {                
+                continue;
+            }
+            if($current != self::EMPTY && !in_array($i, $positions)) {
+                $digitsL = $current . $digitsL;
+                $positions[] = $i;
+                echo "digitsL $digitsL" . PHP_EOL;
+                continue;
+            }
+            if($i === $col) {
+                continue;
+            }
+
+            break;
+        }
+
+        return intval($digitsR) + intval($digitsL);
+
     }
 
     protected function getPath(): string 
